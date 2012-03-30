0c7b1e50b3342b97ae117e3cdecdbe6fbb8eea8a
thomas.hilpold@gmail.com <thomas.hilpold@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2012-03-30 21:04:36 +0000

    the missing shortcut for saveAs(HypergraphFormat)
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2267 43784a7f-e236-0410-9472-c78ae7dffccf

---
+++ apps/owl/gov.miamidade.owlgdb/src/main/java/gov/miamidade/hgowl/plugin/ui/ImportOntologyAction.java
@@ -0,0 +1,48 @@
+package gov.miamidade.hgowl.plugin.ui;
+
+import gov.miamidade.hgowl.plugin.owl.HGOwlEditorKit;
+
+import java.awt.event.ActionEvent;
+
+import org.protege.editor.owl.ui.action.ProtegeOWLAction;
+import org.semanticweb.owlapi.model.OWLOntology;
+
+/**
+ * ImportOntologyAction imports an active file-based ontology into the hypergraphDB repository.
+ * @author Thomas Hilpold (CIAO/Miami-Dade County)
+ * @created Dec 9, 2011
+ */
+public class ImportOntologyAction extends ProtegeOWLAction {
+
+	private static final long serialVersionUID = 12313124211234L;
+
+	/* (non-Javadoc)
+	 * @see org.protege.editor.core.plugin.ProtegePluginInstance#initialise()
+	 */
+	@Override
+	public void initialise() throws Exception {
+	
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.protege.editor.core.Disposable#dispose()
+	 */
+	@Override
+	public void dispose() throws Exception {
+	
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		HGOwlEditorKit hgowlKit = (HGOwlEditorKit) getEditorKit();
+		OWLOntology activeOnto = hgowlKit.getOWLModelManager().getActiveOntology();
+		try {
+			hgowlKit.handleImportRequest(activeOnto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
