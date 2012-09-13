245cec69b071a6e93f122b536788e6a83220d481
thomas.hilpold@gmail.com <thomas.hilpold@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2012-09-13 23:27:03 +0000

    Default Location changed.
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2617 43784a7f-e236-0410-9472-c78ae7dffccf

--- apps/owl/gov.miamidade.owlgdb/src/main/java/gov/miamidade/hgowl/plugin/HGOwlProperties.java
+++ apps/owl/gov.miamidade.owlgdb/src/main/java/gov/miamidade/hgowl/plugin/HGOwlProperties.java
@@ -14,13 +14,13 @@ public class HGOwlProperties {
 
 	    public static final String PREFERENCES_KEY = "HypergraphDB";
 
-	    public static final String HYPERGRAPH_LOCATION_KEY = "HypergraphLocationFolder53-2";
+	    public static final String HYPERGRAPH_LOCATION_KEY = "HypergraphLocationFolderJE-15";
 
 	    public static final String SHOW_LEGACY_KEY = "ShowLegacyEditorKit";
 
 	    private static HGOwlProperties instance;
 
-	    public static final String DEFAULT_HYPERGRAPH_LOCATION_FOLDER_PATH = System.getProperty("user.home") + System.getProperty("file.separator") + ".protegedb53-2";
+	    public static final String DEFAULT_HYPERGRAPH_LOCATION_FOLDER_PATH = System.getProperty("user.home") + System.getProperty("file.separator") + ".protegedb-je-15";
 
 	    private static final boolean DEFAULT_SHOW_LEGACY_EDITOR_KIT = false;
 
